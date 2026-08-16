2e97e10a1fa6335da1ff6459bd369d314fef0519
Diwash Kuskusmiya <diwash.kuskusmiya@gmail.com>
2026-08-15 23:24:17 -0400

    Add: duplicate-integer - submission-13

---
+++ Data Structures & Algorithms/duplicate-integer/submission-13.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        store = defaultdict(int)
+
+        for num in nums:
+            if num in store:
+                return True
+            store[num] = True
+        return False
+
+        
